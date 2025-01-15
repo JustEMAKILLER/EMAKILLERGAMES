05eb7f5f97ca7fcaf3971f26be904a75f9a8329d
JustEMAKILLER <emanuelnuviola@gmail.com>
2025-01-14 22:05:54 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -12,13 +12,17 @@
 function busqueda() {
       const maxPrice = parseFloat(document.getElementById("buscarprecio").value);
       const filtroNombre = document.getElementById("buscarnombre").value.toLowerCase();
-      const products = document.querySelectorAll('.listajuegos li')
+      const products = document.querySelectorAll('.listajuegos li');
+      const images = document.querySelectorAll('img');
       const headers = document.querySelectorAll('h4');
       const text = document.getElementById("texto");
 
       if (isNaN(maxPrice) && filtroNombre === "") {
         for (let i = 0; i < headers.length; i++){
             headers[i].style.display = "block";
+        }
+        for (let i = 0; i < images.length; i++){
+            images[i].style.display = "block";
         }
         for (let i = 0; i < products.length; i++){
             products[i].style.display = "list-item";
@@ -46,6 +50,8 @@ function busqueda() {
           products[i].style.display = mostrar ? "list-item" : "none";
         if (headers[i]){
             headers[i].style.display = "none";}
+        if (images[i]){
+            images[i].style.display = "none";}
         if (!hayResultados){
             text.textContent = "No hay resultados.";
         }
